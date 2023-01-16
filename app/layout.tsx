e79aee3e0c13a26d8b6583ffe8e9e0966815faf4
R-Ram95 <rohinesh@ualberta.ca>
2023-01-16 13:37:22 -0700

    Added main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
         <NavBar />
         <div className="md:pl-52">
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
-            <div className="rounded-md bg-vc-border-gradient p-px shadow-lg shadow-black/20">
-              <div className="rounded-md bg-black p-3.5 md:p-6">{children}</div>
+            <div className="rounded-xl bg-dark-blue p-3.5 md:p-6 drop-shadow-2xl  border border-blue-100">
+              {children}
             </div>
           </div>
         </div>
--- ui/NavBar.tsx
+++ ui/NavBar.tsx
@@ -1,52 +1,40 @@
-import Image from "next/image";
-import logo from "../public/starwars-logo.png";
+"use client";
 import { BsFilm, BsPeopleFill } from "react-icons/bs";
 import { BiPlanet, BiDna } from "react-icons/bi";
 import { FaSpaceShuttle } from "react-icons/fa";
-import { AiFillCar, AiFillHome } from "react-icons/ai";
+import { AiFillCar } from "react-icons/ai";
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 const navItems = [
   {
-    name: "Home",
-    slug: "/",
-    current: true,
-    icon: AiFillHome,
-  },
-  {
     name: "Films",
-    slug: "/films",
-    current: false,
+    slug: "films",
     icon: BsFilm,
   },
   {
     name: "People",
-    slug: "/people",
-    current: false,
+    slug: "people",
     icon: BsPeopleFill,
   },
   {
     name: "Planets",
-    slug: "/planets",
-    current: false,
+    slug: "planets",
     icon: BiPlanet,
   },
   {
     name: "Species",
-    slug: "/species",
-    current: false,
+    slug: "species",
     icon: BiDna,
   },
   {
     name: "Starships",
-    slug: "/starships",
-    current: false,
+    slug: "starships",
     icon: FaSpaceShuttle,
   },
   {
     name: "Vehicles",
-    slug: "/vehicles",
-    current: false,
+    slug: "vehicles",
     icon: AiFillCar,
   },
 ];
@@ -59,34 +47,44 @@ export function NavBar() {
   return (
     <div className="fixed top-0 z-10 flex w-full flex-col border-b border-blue-100 bg-dark-blue md:bottom-0 md:z-auto md:w-52 md:border-r md:border-blue-100">
       <div className="flex h-16 flex-shrink-0 items-center bg-yellow px-4">
-        <Image className="h-8 w-20" src={logo} alt="Star Wars Wiki" />
+        <Link className="flex flex-row space-x-5 items-center pt-5" href={"/"}>
+          <h1 className="font-extrabold font-mono text-transparent text-2xl bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-yellow-500 ">
+            StarWars <br /> Wikipedia
+          </h1>
+        </Link>
       </div>
       <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
         <nav className="mt-5 flex-1 space-y-1 px-2" aria-label="Sidebar">
-          {navItems.map((item) => (
-            <Link
-              key={item.name}
-              href={item.slug}
-              className={classNames(
-                item.current
-                  ? "bg-light-blue text-white"
-                  : "text-gray-300 hover:bg-light-blue hover:text-white",
-                "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-              )}
-            >
-              <item.icon
-                className={classNames(
-                  item.current
-                    ? "text-gray-300"
-                    : "text-gray-400 group-hover:text-gray-300",
-                  "mr-4"
-                )}
-              />{" "}
-              {item.name}
-            </Link>
-          ))}
+          {navItems.map((item) => NavItem(item))}
         </nav>
       </div>
     </div>
+  );
+}
+
+function NavItem(item: any): any {
+  const segement = useSelectedLayoutSegment();
+  let isActive: boolean = segement === item.slug;
+  return (
+    <Link
+      key={item.name}
+      href={`/${item.slug}`}
+      className={classNames(
+        isActive
+          ? "bg-light-blue text-white"
+          : "text-gray-300 hover:bg-blue-600 hover:text-white",
+        "group flex items-center px-2 py-2 text-md font-medium rounded-md"
+      )}
+    >
+      <item.icon
+        className={classNames(
+          isActive
+            ? "text-gray-300"
+            : "text-gray-400 group-hover:text-gray-300",
+          "mr-4"
+        )}
+      />
+      {item.name}
+    </Link>
   );
 }
